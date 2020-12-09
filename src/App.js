c81e5d3a536052bdafcbb2a06591a98e20c7df1f
GrayHead <ss.zhuravlov@gmail.com>
2020-12-09 21:01:07 +0200

    lesson 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
 
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+} from 'react-router-dom';
+import AllUsers from './components/all-users/AllUsers';
 
 class App extends Component {
 
@@ -7,7 +14,22 @@ class App extends Component {
 	render() {
 
 		return (
-			<div></div>
+			<Router>
+				<div>
+					<ul>
+						<li>
+							<Link to={'/users'}>to all users</Link>
+						</li>
+					</ul>
+					<hr/>
+					<Switch>
+						<Route path={'/users'} render={() => <AllUsers/>}/>
+					</Switch>
+					<hr/>
+
+
+				</div>
+			</Router>
 		);
 	}
 }
