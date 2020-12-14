334209a930f6be13544abcfc92f86bd1bd0d6e83
GrayHead <ss.zhuravlov@gmail.com>
2020-12-14 22:16:46 +0200

    forms

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React, {Component} from 'react';
 
 
 class App extends Component {
+	myForm = React.createRef();
 
+	state = {inputValue: ''};
 
 	render() {
 
 		return (
-			<div></div>
+			<div>
+				<form action={'/savedata'} onSubmit={this.send} ref={this.myForm}>
+					<input type='number' onInput={this.commitState} value={this.state.inputValue}/>
+					<button>send</button>
+				</form>
+
+			</div>
 		);
 	}
+
+	send = (e) => {
+		e.preventDefault();
+		// console.log(this.myForm.current[0].value);
+		console.log(e.target.children[0].value);
+
+	};
+
+	commitState = (e) => {
+		this.setState({inputValue: e.target.value});
+
+	};
 }
 
 export default App;
